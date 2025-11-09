62a1c65b26fed46ebc9feb83ed5a36ab401c9582
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2025-11-08 19:43:17 -0500

    add pythong solution for adding 2 numbers in linkedlist (medium), 02 and 445

--- python/leetcode/easy/83_remove_duplicates_from_sorted_list.py
+++ python/leetcode/easy/83_remove_duplicates_from_sorted_list.py
@@ -19,6 +19,7 @@ class ListNode(object):
 
 class Solution(object):
     
+    # this is one ptr solution that removes only consecutive duplicates
     def deleteDuplicates_itiration1(self, head):
         current = head
         ptr = head
@@ -38,6 +39,35 @@ class Solution(object):
                 current.next=current.next.next
             else:
                 current=current.next
+        return head
+    
+    # removes any duplicates
+    # O(n2) solution
+    def delete_duplicates_2ptrs(self, head):
+        ptr1 = head
+        while ptr1:
+            ptr2 = ptr1
+            while ptr2.next:
+                if ptr2.next.val == ptr1.val:
+                    ptr2.next = ptr2.next.next
+                else:
+                    ptr2 = ptr2.next
+            ptr1 = ptr1.next
+        return head
+    
+    #using set
+    # O(n) time and O(n) space
+    def remove_duplicates_set(self, head):
+        values = set()
+        prev = None
+        current = head
+        while current:
+            if current.val in values:
+                prev.next = current.next
+            else:
+                values.add(current.val)
+                prev = current
+            current = current.next
         return head
 
 if __name__ == "__main__":
