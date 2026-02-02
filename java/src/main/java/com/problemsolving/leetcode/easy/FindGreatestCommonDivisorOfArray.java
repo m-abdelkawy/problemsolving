dfde38ab049c5bb65f1667959cd0a58b050c3370
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-02-02 14:48:17 -0500

    feat: solve GCD of array using Euclidean algorithm

---
+++ java/src/main/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,4 @@
+package com.problemsolving.leetcode.easy;
+
+public class FindGreatestCommonDivisorOfArray {
+}
---
+++ java/src/test/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArrayTest.java
@@ -0,0 +1,12 @@
+package com.problemsolving.leetcode.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindGreatestCommonDivisorOfArrayTest {
+
+    @Test
+    void findGCD() {
+    }
+}
