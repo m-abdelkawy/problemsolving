a880119fcf4f7c8e3a4bf012f4b669c52568e01b
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-02-02 14:48:35 -0500

    feat: solve GCD of array using Euclidean algorithm

--- java/src/main/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArray.java
+++ java/src/main/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArray.java
@@ -1,4 +1,30 @@
 package com.problemsolving.leetcode.easy;
 
 public class FindGreatestCommonDivisorOfArray {
+    public int findGCD(int[] nums){
+        int[] maxMin = findMinMax(nums);
+        return compute_gcd(maxMin[0], maxMin[1]);
+    }
+
+    private int[] findMinMax(int[] nums){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : nums){
+            min = Math.min(num, min);
+            max = Math.max(num, max);
+        }
+        int[] res = new int[2];
+        res[0] = max;
+        res[1] = min;
+        return res;
+    }
+
+    private int compute_gcd(int a, int b){
+        if(b == 0) return a;
+        return compute_gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- java/src/test/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArrayTest.java
+++ java/src/test/java/com/problemsolving/leetcode/easy/FindGreatestCommonDivisorOfArrayTest.java
@@ -7,6 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class FindGreatestCommonDivisorOfArrayTest {
 
     @Test
-    void findGCD() {
+    void findGCDOfTypicalArray(){
+        FindGreatestCommonDivisorOfArray solver = new FindGreatestCommonDivisorOfArray();
+        int[] nums = new int[]{2, 5, 6, 9, 10};
+        int result = solver.findGCD(nums);
+        assertEquals(2, result);
+    }
+
+    @Test
+    void findsGcdWhenAllNumbersShareFactor() {
+        FindGreatestCommonDivisorOfArray solver =
+                new FindGreatestCommonDivisorOfArray();
+
+        int[] nums = {8, 16, 32, 64};
+        int result = solver.findGCD(nums);
+
+        assertEquals(8, result);
     }
 }
