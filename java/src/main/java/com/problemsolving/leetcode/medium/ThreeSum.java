a4cd5c5772cd1de3112373717a2f083bc79c0c83
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-03-26 11:09:48 -0400

    feat: re-solve 15. 3Sum

--- java/src/main/java/com/problemsolving/leetcode/medium/ThreeSum.java
+++ java/src/main/java/com/problemsolving/leetcode/medium/ThreeSum.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ * 15. 3Sum
  * Link: https://leetcode.com/problems/3sum/
  * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
  * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
@@ -11,9 +12,17 @@ import java.util.stream.Collectors;
  * Notice that the solution set must not contain duplicate triplets.
  *
  * @author Mohammed Abdelkawy
- * @version 1.0.0 - 31.05.2022
+ * @version 1.0.0
+ * @since 31.05.2022
  */
 public class ThreeSum {
+    /**
+     * Space Complexity: O(1) -> no extra DS, ignoring output list
+     * Time Complexity: O(n^2) -> O(n log n) for soring + O(n^2) for outer loop plus 2 pointers
+     * @param numbers
+     * @param target
+     * @return
+     */
     public List<List<Integer>> threeSum1(int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
@@ -49,30 +58,104 @@ public class ThreeSum {
     }
 
     /**
-     * Hashset approach
-     *
-     * @param nums
+     * Space Complexity: O(1) -> no extra DS, ignoring output list
+     * Time Complexity: O(n^2) -> O(n log n) for soring + O(n^2) for outer loop plus 2 pointers
+     * @param numbers
+     * @param target
      * @return
      */
     public List<List<Integer>> threeSum2(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> triplets = new ArrayList<>();
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                twoSumHashSet(nums, i, triplets);
+        Arrays.sort(nums);
+        int N = nums.length;
+        for (int i = 0; i < N; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = N - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while (j < k && nums[j + 1] == nums[j]) {
+                        j++;
+                    }
+                    while (j < k && nums[k - 1] == nums[k]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+
+
+                } else if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                }
             }
         }
         return triplets;
     }
 
-    private void twoSumHashSet(int[] nums, int firstIndex, List<List<Integer>> lst) {
+    /**
+     * Hashset approach
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triplets = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumHashSet1(nums, i, triplets);
+            }
+        }
+        return triplets;
+    }
+
+    private void twoSumHashSet1(int[] nums, int firstIndex, List<List<Integer>> lst) {
         Set<Integer> seen = new HashSet<>();
         for (int i = firstIndex + 1; i < nums.length; i++) {
             int complement = -nums[firstIndex] - nums[i];
             if(seen.contains(complement)){
                 lst.add(List.of(nums[firstIndex], nums[i], complement));
                 while (i + 1 < nums.length && nums[i+1] == nums[i]){
+                    i++;
+                }
+            }
+            seen.add(nums[i]);
+        }
+    }
+
+    /*-----------*/
+
+    /**
+     * Space Complexity: O(n) -> for introducing the seen hashset, that is created every outer iteration and holding n numbers at maximum [excluding the output list]
+     * Time Complexity: O(n^2) -> for the outer loop x the inner hash scan
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum4(int[] nums) {
+        int N = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            twoSumHashSet2(nums, i, res);
+        }
+        return res;
+    }
+
+    private void twoSumHashSet2(int[] nums, int firstIndex, List<List<Integer>> lst){
+        int N = nums.length;
+        Set<Integer> seen = new HashSet<>();
+        for (int i = firstIndex + 1; i < N; i++) {
+            int first = nums[firstIndex];
+            int complement = 0-first-nums[i];
+            if(seen.contains(complement)){
+                lst.add(Arrays.asList(first, nums[i], complement));
+                while(i + 1 < N && nums[i+1] == nums[i]){
                     i++;
                 }
             }
