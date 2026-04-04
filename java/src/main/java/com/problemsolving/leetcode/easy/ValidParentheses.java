a3d62d2feadbbbf5220224e16449049b0e054374
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-04-04 12:21:05 -0400

    feat: re-solve 20. Valid Parentheses

--- java/src/main/java/com/problemsolving/leetcode/easy/ValidParentheses.java
+++ java/src/main/java/com/problemsolving/leetcode/easy/ValidParentheses.java
@@ -7,8 +7,8 @@ import java.util.*;
  * Link: https://leetcode.com/problems/valid-parentheses/
  *
  * @author Mohammed Abdelkawy
- * @version 1.0.0
- * @since 25.08.2022
+ * @version 2.0.0
+ * @since 04.04.2026
  */
 public class ValidParentheses {
     public static boolean isValid(String s) {
@@ -77,6 +77,49 @@ public class ValidParentheses {
             }
         }
 
+        return stack.isEmpty();
+    }
+
+    /**
+     * solve using the Deque packed by the ArrayDeque class
+     * Time Complexity : O(n) for each char is checked once
+     * Space Complexity: O(n) for the stack holds at max n values equal to the length of the input string
+     * @param s
+     * @return
+     */
+    public boolean isValid4(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            } else if(!stack.isEmpty() &&
+                    ((c == ')' && stack.peek() == '(') || (c == ']' && stack.peek() == '[') || (c == '}' && stack.peek() == '{'))){
+                stack.pop();
+            } else {return false;}
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * solve using the Deque packed by the ArrayDeque class
+     * Time Complexity : O(n) for each char is checked once
+     * Space Complexity: O(n) for the stack holds at max n values equal to the length of the input string
+     * @param s
+     * @return
+     */
+    public boolean isValid5(String s) {
+        // map of <Closing, Opening>
+        Map<Character, Character> pairs = Map.of(')', '(', ']', '[', '}', '{');
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!pairs.containsKey(c)){
+                stack.push(c);
+            } else if (!stack.isEmpty() && stack.peek() == pairs.get(c)){
+                stack.pop();
+            } else return false;
+        }
         return stack.isEmpty();
     }
 
