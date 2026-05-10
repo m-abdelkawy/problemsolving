3f5e2940fa8dec55cd2a68ca734161a4cb591c80
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2026-05-10 17:22:51 -0400

    solve 278. First Bad Version

--- java/src/main/java/com/problemsolving/leetcode/easy/FirstBadVersion.java
+++ java/src/main/java/com/problemsolving/leetcode/easy/FirstBadVersion.java
@@ -9,5 +9,41 @@ package com.problemsolving.leetcode.easy;
  * @since 10.12.2022
  */
 public class FirstBadVersion {
+    /**
+     * Time Complextiy: O(log n)
+     * Space Complexity: O(1)
+     * @param n
+     * @return
+     */
+    public int firstBadVersion1(int n) {
+        int lo = 1, hi = n;
+        int firstBadVersion = -1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(isBadVersion(mid)){
+                firstBadVersion = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return firstBadVersion;
+    }
 
+    public int firstBadVersion2(int n) {
+        int lo = 0, hi = n;
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+            if(isBadVersion(mid)){
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    private boolean isBadVersion(int version) {
+        return true;
+    }
 }
